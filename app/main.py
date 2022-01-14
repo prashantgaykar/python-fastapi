ccbeb2a0daaf50d7c31ddf6923c1d8711272632a
prashant.gaykar@redmangoanalytics.com <pg7233c%>
2022-01-14 22:27:02 +0530

    added test post api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+from typing import Any, Dict
 from fastapi import FastAPI
 from .database import init_models
-from .routers import post,user,auth,test
+from .routers import post, user, auth, test
 
 init_models()
 app = FastAPI()
@@ -10,7 +11,13 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(test.router)
 
+
 @app.get("/")
 async def root():
     return "Hello World !"
 
+
+@app.post("/test")
+def test_post(test_data: Dict = dict()):
+    print("Test Data : ", test_data)
+    return "Success!"
